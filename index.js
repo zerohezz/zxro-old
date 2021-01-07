e4e7c90323e5fc9b791adf08f3e135417f77f8e9
Anthony DeSilva <anthonydesilva1111@gmail.com>
2021-01-07 16:14:03 -0500

    Embeds

--- index.js
+++ index.js
@@ -7,9 +7,17 @@ const command = require('./command')
 client.on('ready', () => {
     console.log('The client is ready!')
 
-    command(client, ['ping', 'hi', 'test'], (message) => {
+    command(client, ['ping'], (message) => {
         message.channel.send('whts up')
     })
+    
+    command(client,'embed', (message) => { 
+        const embed = new Discord.MessageEmbed()
+        .setTitle('Example text embed')
+
+        messsage.channel.send(embed)
+    })
+
 })
 
 client.login(process.env.DJS_TOKEN)
